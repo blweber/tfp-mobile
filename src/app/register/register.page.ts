ce1422b236470ec6662e7612e17efc555e318f83
Weber <britweber@ussarrbritweber3.www.eenews.com>
2019-01-29 22:08:28 -0500

    print name to console

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  
+  public name : string;
+  public password : string;
 
-  constructor() { }
+  constructor( public navCtrl: NavController ) { }
 
   ngOnInit() {
   }
 
+  logName() {
+    if (this.password == "") {
+      alert("Really? No password...?");
+    } else {
+      alert("Cool");
+    }
+    console.log(this.name)
+    console.log(this.password)
+  }
 }
